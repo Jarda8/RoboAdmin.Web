008142f1bc9d5f2d023816c64ade964054e476d6
Jarda8 <jaroslav1.sevcik@gmail.com>
2015-11-27 16:02:20 +0100

    přidán CKeditor pro editovatelná textová pole a +- dodělán event-detail bez stylování

--- app/views/events/event-detail.js
+++ app/views/events/event-detail.js
@@ -2,7 +2,15 @@
 	return function (id) {
             return new kendo.View(
                 "event-detail",
-                { model: kendo.observable({ id: id }) }
+                {
+                    model: kendo.observable({ id: id }),
+                    
+                    init: function (e) {
+                        CKEDITOR.replace( 'description-cz' );
+                        CKEDITOR.replace( 'description-en' );
+                        CKEDITOR.replace( 'solution' );
+                    }
+                }
         );};
 });
 
