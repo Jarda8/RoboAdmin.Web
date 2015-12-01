20841c34c0f2613f65dedf8b2b4f3109b75117d6
TomasTvrz <tomas.tvrz007@gmail.com>
2015-12-01 12:47:06 +0100

    Server detail
    a vše co se ho týká

---
+++ app/views/disks/disk-settings.js
@@ -0,0 +1,14 @@
+﻿define(["kendo"], function() {
+	return function (id) {
+            return new kendo.View(
+                "disk-settings",
+                {
+                    model: kendo.observable({ id: id }),
+                    
+                    init: function (e) {
+                        //CKEDITOR.replace( 'description-original' );
+                    }
+                }
+        );};
+});
+
---
+++ app/views/servers/server-factors.js
@@ -0,0 +1,14 @@
+﻿define(["kendo"], function() {
+	return function (id) {
+            return new kendo.View(
+                "server-factors",
+                {
+                    model: kendo.observable({ id: id }),
+                    
+                    init: function (e) {
+                        //CKEDITOR.replace( 'description-original' );
+                    }
+                }
+        );};
+});
+
